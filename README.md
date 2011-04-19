f2fce8f1317bc76d5653cbc1c2739f29ab432b01
Johannes Wagener <johannes@soundcloud.com>
2011-04-19 16:01:09 +0200

    remove code, default for record arg, add example

---
+++ README.md
@@ -0,0 +1,4 @@
+RecButton provides a flash based audio recording and uploading functionality through a simple JavaScript API.
+This is still in beta. Better docs and examples will be added. Have a look at example-1.html to check out how it works. 
+(needs to be accessed through HTTP not the local filesystem due to flash restrictions).
+
--- RecButton/RecButton.js
+++ RecButton/RecButton.js
@@ -15,6 +15,7 @@ var RecButton = {
   },
 
   record: function(options){
+    options = options || {};
     this.bind('recordingStart', options['onStart']);
     this.flashInterface().startRecording();
   },
@@ -48,46 +49,3 @@ var RecButton = {
     return this.swfObject.startRecording ? this.swfObject : this.swfObject.children[3];
   },
 };
-
-
-function Recorder(swfObject){
-   this.swfObject   = swfObject;
-   this.isPlaying   = false;
-   this.isRecording = false;
-   
-   this.send = function(message){
-     return swfObject.sendToActionScript(message);
-   };
-   
-   this.upload = function(uri){
-     return this.send('upload ' + uri);
-   };
-   
-   this.setup = function(){
-     return this.send('setup');
-   };
-   
-   this.startRecording = function(){
-     this.isRecording = true;
-     return this.send('startRecording');
-   };
-   
-   this.stopRecording = function(){
-     this.isRecording = false;
-     return this.send('stopRecording');
-   };
-   
-   this.startPlaying = function(){
-     this.isPlaying = true;
-     return this.send('startPlaying');
-   };
-   
-   this.stopPlaying = function(){
-     this.isPlaying = false;
-     return this.send('stopPlaying');
-   };
-   
-   this.getDebugLog = function(){
-     return this.send('getDebugLog');
-   };
-}
